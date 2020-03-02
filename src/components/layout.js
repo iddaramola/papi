f4566f2207432c4e843f509826287b1d5eecedfe
Idowu Daramola <idowu.daramola@gmail.com>
2020-03-03 00:13:13 +0100

    changed the default, ready for CD

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+     { /* 
+        <Header siteTitle={data.site.siteMetadata.title} />
+     */} 
       <div
         style={{
           margin: `0 auto`,
@@ -35,9 +37,11 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+        {/* 
+        © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
+      */} 
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,18 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hello!</h1>
+    <p>Welcome to Papitrends</p>
+    {/*
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+        <Image />  
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    */}
+    
+    {/*
+  <Link to="/page-2/">Go to page 2</Link>
+  */}
   </Layout>
 )
 
