b0691f0649641dbb1d5904bca293fabe74614ff2
Idowu Daramola <idowu.daramola@gmail.com>
2020-03-03 01:31:17 +0100

    changed the site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Papitrends Store`,
+    description: `Pick up original African style blend with Western flavor: attires you can get from nowhere else`,
+    author: `@iddaramola`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
