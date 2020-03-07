22921f9144136a395d05774fee0e4d7ad840d3dd
Idowu Daramola <idowu.daramola@gmail.com>
2020-03-07 09:48:24 +0100

    edited header and layout to match manifest color

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
-     { /* 
+     {  
         <Header siteTitle={data.site.siteMetadata.title} />
-     */} 
+     } 
       <div
         style={{
           margin: `0 auto`,
